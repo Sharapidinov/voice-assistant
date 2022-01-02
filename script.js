18f162e78b8f56dea2de0a49df1926c4dbb1debb
Sharapidinov <austie2204@gmail.com>
2022-01-02 21:18:46 +0600

    add new function

--- script.js
+++ script.js
@@ -20,12 +20,14 @@ recognition.addEventListener("result", (e) => {
             p = document.createElement("p")
             p.classList.add("replay")
             p.innerText = "Отлично, а у вас как дела?"
+            speechSynthesis.speak(new SpeechSynthesisUtterance('Отлично, а у вас как дела?'))
             texts.appendChild(p)
         }
         if (text.includes("Привет") || text.includes("привет")) {
             p = document.createElement("p")
             p.classList.add("replay")
             p.innerText = "Здравствуйте!"
+            speechSynthesis.speak(new SpeechSynthesisUtterance('Здравствуйте!'))
             texts.appendChild(p)
         }
         if (
@@ -35,6 +37,7 @@ recognition.addEventListener("result", (e) => {
             p = document.createElement("p")
             p.classList.add("replay")
             p.innerText = "Мое имя Шелли"
+            speechSynthesis.speak(new SpeechSynthesisUtterance('Мое имя Шелли'))
             texts.appendChild(p)
         }
         if (
@@ -44,12 +47,14 @@ recognition.addEventListener("result", (e) => {
             p = document.createElement("p")
             p.classList.add("replay")
             p.innerText = "Мой создатель Темирлан"
+            speechSynthesis.speak(new SpeechSynthesisUtterance('Мой создатель Темирлан'))
             texts.appendChild(p)
         }
         if (text.includes("Открой YouTube") || text.includes("открой YouTube")) {
             p = document.createElement("p")
             p.classList.add("replay")
             p.innerText = "Открываю YouTube"
+            speechSynthesis.speak(new SpeechSynthesisUtterance('Открываю YouTube'))
             texts.appendChild(p)
             console.log("opening youtube")
             window.open("https://www.youtube.com")
